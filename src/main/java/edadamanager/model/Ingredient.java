9d81111aae37af7ac689406bb09b1bf9ee8477e9
Ekaterina Ponomareva <katya@pvv38.ru>
2020-05-16 11:07:05 +0800

    Прикрутила БД

--- src/main/java/edadamanager/model/Ingredient.java
+++ src/main/java/edadamanager/model/Ingredient.java
@@ -1,16 +1,28 @@
 package edadamanager.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Component
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
     private Integer calory;
-    private Units units;
+//    private Units units;
 }
